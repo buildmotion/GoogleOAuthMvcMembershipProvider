c8c48e3dbb538c727897cafe902f7e3479017906
buildmotion <matt.vaughn@buildmotion.com>
2013-09-27 22:21:59 -0600

    removed license header from files

--- BuildMotion.Membership/Business/Repository.cs
+++ BuildMotion.Membership/Business/Repository.cs
@@ -1,15 +1,4 @@
-﻿//------------------------------------------------------------------------------
-// <Vergosity.License>
-//    All of the source code, logic, patterns, notes...really anything contained in the 
-//		source code, compiled assemblies, or other mechanisms (i.e., drawings, diagrams, 
-//		notes, or documentation) are the sole and explicit property of Build Motion, LLC.
-//
-//    You are entitled to use the compiled representations of the software only if they 
-//		are licensed by either Vergosity or Build Motion, LLC. See "License.txt" in compiled
-//		resource for details on license limitations and usage agreement.
-// </Vergosity.License>
-//------------------------------------------------------------------------------
-#region
+﻿#region
 
 using System;
 using System.Collections.Generic;
--- BuildMotion.Membership/Business/Security/Attributes/UserInformationIsValidAttribute.cs
+++ BuildMotion.Membership/Business/Security/Attributes/UserInformationIsValidAttribute.cs
@@ -1,14 +1,3 @@
-//------------------------------------------------------------------------------
-// <Vergosity.License>
-//    All of the source code, logic, patterns, notes...really anything contained in the 
-//		source code, compiled assemblies, or other mechanisms (i.e., drawings, diagrams, 
-//		notes, or documentation) are the sole and explicit property of Build Motion, LLC.
-//
-//    You are entitled to use the compiled representations of the software only if they 
-//		are licensed by either Vergosity or Build Motion, LLC. See "License.txt" in compiled
-//		resource for details on license limitations and usage agreement.
-// </Vergosity.License>
-//------------------------------------------------------------------------------
 #region
 
 using BuildMotion.Membership.Business.Security.Rules;
